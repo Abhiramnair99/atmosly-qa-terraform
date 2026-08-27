6e9b61d59507734ac8901e6dc738818a39eb5089
Abhiram Nair <kushagr.jain@squareops.com>
2026-08-27 15:55:19 +0530

    QA case 5: commit B - pushed after the plan was built, to make that plan stale

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_s3_bucket" "qa" {
     ManagedBy = "atmosly"
     Purpose   = "infra-management-qa"
     Owner     = var.owner
-    CaseThree = "case5-plan-A"
+    CaseThree = "case5-commit-B"
   }
 }
 
